f39de8fa13ebbeef4adebfea5bdffa03a09b5d02
rovshanguli <rovshanshg@code.edu.az>
2021-12-02 00:00:03 +0400

    Checked

---
+++ CheckLogin/CheckLogin/Check.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckLogin
+{
+    class Check
+    {
+        public Check()
+        {
+            Console.WriteLine("Login inputlarinin yoxlanmasi ");
+        }
+        public Check(string name , int password):this()
+        {
+            if(name== "cavid@code.edu.az" && password == 12345)
+            {
+                Console.WriteLine("Giris Olundu");
+            }
+            else
+            {
+                Console.WriteLine("Mail ve ya Password sehvdir");
+            }
+
+        }
+    }
+}
---
+++ CheckLogin/CheckLogin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CheckLogin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Check login = new Check("cavid@code.edu.az", 12345);
+        }
+    }
+}
