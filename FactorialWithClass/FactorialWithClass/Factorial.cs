65396fa50876efc8b367a91b4fbc92a930c6b63c
rovshanguli <rovshanshg@code.edu.az>
2021-12-01 23:33:54 +0400

    Factroial

---
+++ FactorialWithClass/FactorialWithClass/Factorial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactorialWithClass
+{
+    class Factorial
+    {
+        public Factorial(int n)
+        {
+            int fuc = 1;
+            for(int i= 1; i < n; i++)
+            {
+                fuc = fuc* i;
+            }
+            Console.WriteLine(fuc);
+        }
+    }
+}
---
+++ FactorialWithClass/FactorialWithClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FactorialWithClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factorial fac = new Factorial(6);
+        }
+    }
+}
