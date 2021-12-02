315cba157f5dd52fd615baf245eca605c29cf76b
rovshanguli <rovshanshg@code.edu.az>
2021-12-02 15:14:58 +0400

    Fixed Bug

--- NoteBooks(hierarchy)/NoteBooks(hierarchy)/HP.cs
+++ NoteBooks(hierarchy)/NoteBooks(hierarchy)/HP.cs
@@ -4,5 +4,14 @@ using System.Text;
 
 namespace NoteBooks_hierarchy_
 {
-    
+    class HP:NoteBooks
+    {
+        public HP(int ram, string processor, double screensize, int memory) : base(ram, processor, screensize, memory)
+        {
+            this.ram = ram;
+            this.processor = processor;
+            this.screensize = screensize;
+            this.memory = memory;
+        }
+    }
 }
--- NoteBooks(hierarchy)/NoteBooks(hierarchy)/Program.cs
+++ NoteBooks(hierarchy)/NoteBooks(hierarchy)/Program.cs
@@ -10,9 +10,15 @@ namespace NoteBooks_hierarchy_
             Asus notebook = new Asus(16,"Intel i5",15.6,512);
 
             Console.WriteLine("                                              ");
+
             Console.WriteLine("Ryzen Parametrs   :");
             Acer notebook1 = new Acer(8, "Ryzen r3", 13.8, 1024);
-           
+
+            Console.WriteLine("                                               ");
+
+            Console.WriteLine("HP parametrs");
+            HP notebook2 = new HP(32,"Ryzen r7",17.3,256);
+
            
         }
         
